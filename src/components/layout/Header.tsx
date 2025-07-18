fc4035ef244b6198a8de4e7229ede2c979b48794
arjin <arjinalbay21@gmail.com>
2025-07-18 22:29:52 +0300

    refactor(Header): remove notification bell component for cleaner layout

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -101,18 +101,7 @@ export default function Header() {
           {/* Notification Center */}
           <NotificationCenter />
 
-           {/* Notification Bell */}
-          <Button variant="ghost" size="icon" className="relative">
-            <Bell className="h-5 w-5" />
-            {unreadCount > 0 && (
-              <Badge
-                variant="destructive"
-                className="absolute -top-2 -right-2 h-5 w-5 rounded-full p-0 text-xs"
-              >
-                {unreadCount > 9 ? '9+' : unreadCount}
-              </Badge>
-            )}
-          </Button>
+      
           
           {/* User Menu */}
           <DropdownMenu>
